c3e0f5dbb552cbc9854c8dbc1d0efb9f48b0f95c
sagan5 <43873645+sagan5@users.noreply.github.com>
2020-06-01 22:29:13 +0300

    typo fix

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use("/api/posts", posts);
 // server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // static folder
-  app.user(express.static("client/build"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
